b235d921d9517c7e8df67ce0d2a63b3e29e0ec5e
올리비아 <olivia@ollibiaui-MacBookPro.local>
2022-11-13 18:40:44 +0900

    새로운 섹션 추가

--- js/olivia.js
+++ js/olivia.js
@@ -1,13 +1,15 @@
 window.onload = function(){
     var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 3,
-        spaceBetween: 50,
-        slidesPerGroup: 1,
+        slidesPerView: 2,
+        spaceBetween: 30,
         loop: true,
         loopFillGroupWithBlank: true,
         pagination: {
           el: ".swiper-pagination",
           clickable: true,
+          renderBullet: function (index, className) {
+            return `<span class="bg-light border border-light px-2 w-auto h-auto btns rounded-1 swiper-pagination-bullet">${index + 1}</span>`;
+            },
         },
         navigation: {
           nextEl: ".swiper-button-next",
